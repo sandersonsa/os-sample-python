6a9fcbd43f422cb2b3dad66b198ba484d5b4bacd
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2024-10-25 11:27:17 -0300

    client llm

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
+from kafka import KafkaConsumer
 from flask import request
-application = Flask(__name__)
+import json
 
-@application.route("/")
-def hello():
-    print(request.headers)  
+app = Flask(__name__)
+
+# Separate Kafka client logic
+class KafkaClient:
+    def __init__(self, bootstrap_servers, topic):
+        self.consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=bootstrap_servers,
+            auto_offset_reset='earliest',
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+        )
+
+    def consume_messages(self):
+        messages = []
+        for message in self.consumer:
+            messages.append(message.value)
+        return messages
+
+# Flask endpoint
+@app.route('/process', methods=['GET'])
+def process():
+    # Kafka configuration
+    kafka_bootstrap_servers = 'amq-streams-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092'  # Replace with your Kafka broker address
+    kafka_topic = 'quarkus-topic'
+
+    # Initialize Kafka client
+    kafka_client = KafkaClient(kafka_bootstrap_servers, kafka_topic)
+
+    # Consume messages from Kafka
+    messages = kafka_client.consume_messages()
+    for message in messages:
+        print(message.value.decode())
+    # return jsonify(messages)
     return "Hello World!"
 
-if __name__ == "__main__":
-    application.run( host='0.0.0.0', port=8080 )
+if __name__ == '__main__':
+    app.run(debug=True)
