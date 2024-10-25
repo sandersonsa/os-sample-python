1a961dc3285d028860820363be0b526b783cb860
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2024-10-25 12:09:46 -0300

    client llm - 1

--- wsgi.py
+++ wsgi.py
@@ -11,32 +11,44 @@ class KafkaClient:
         self.consumer = KafkaConsumer(
             topic,
             bootstrap_servers=bootstrap_servers,
-            auto_offset_reset='earliest',
-            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+            auto_offset_reset='earliest'
         )
 
     def consume_messages(self):
-        messages = []
         for message in self.consumer:
-            messages.append(message.value)
-        return messages
+            print(message.value.decode())
+        return "Olá Mundo"
+    
+    # def consume_messages(self):
+    #     messages = []
+    #     for message in self.consumer:
+    #         messages.append(message.value.decode())
+    #     return messages
 
 # Flask endpoint
 @app.route('/process', methods=['GET'])
 def process():
     # Kafka configuration
+    # kafka_bootstrap_servers = 'localhost:9092'  # Replace with your Kafka broker address
     kafka_bootstrap_servers = 'amq-streams-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092'  # Replace with your Kafka broker address
     kafka_topic = 'quarkus-topic'
 
+
+    consumer = KafkaConsumer(kafka_topic, bootstrap_servers=[kafka_bootstrap_servers])
+    for message in consumer:
+        print(message.value.decode())
+
+
     # Initialize Kafka client
-    kafka_client = KafkaClient(kafka_bootstrap_servers, kafka_topic)
+    # kafka_client = KafkaClient(kafka_bootstrap_servers, kafka_topic)
 
     # Consume messages from Kafka
-    messages = kafka_client.consume_messages()
-    for message in messages:
-        print(message.value.decode())
+    # messages = kafka_client.consume_messages()
+    # print(messages)
+    # for message in messages:
+    #     print(message.value.decode())
     # return jsonify(messages)
     return "Hello World!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run( host='0.0.0.0', port=8080 )
