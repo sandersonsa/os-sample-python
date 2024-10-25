b584114b16c2c4bc524711a15c83bc99645b4e28
C1343636 Sanderson Costa De Sa <c1343636@interno.bb.com.br>
2024-10-25 16:41:15 -0300

    client llm 9

--- wsgi.py
+++ wsgi.py
@@ -5,39 +5,34 @@ import json
 
 application = Flask(__name__)
 
-# Separate Kafka client logic
-class KafkaClient:
-    def __init__(self, bootstrap_servers, topic):
-        self.consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=bootstrap_servers,
-            auto_offset_reset='earliest'
-        )
-
-    def consume_messages(self):
-        for message in self.consumer:
-            print(message.value.decode())
-        return "Olá Mundo"
-    
-    # def consume_messages(self):
-    #     messages = []
-    #     for message in self.consumer:
-    #         messages.append(message.value.decode())
-    #     return messages
-
 # Flask endpoint
 @application.route('/process', methods=['GET'])
 def process():
     # Kafka configuration
-    # kafka_bootstrap_servers = 'localhost:9092'  # Replace with your Kafka broker address
-    kafka_bootstrap_servers = 'amq-streams-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092'  # Replace with your Kafka broker address
-    kafka_topic = 'quarkus-topic'
+    # kafka_bootstrap_servers = 'amq-streams-kafka-bootstrap.kafka-cluster.svc.cluster.local:9092'  # Replace with your Kafka broker address
+    # kafka_topic = 'quarkus-topic'
 
+    # consumer = KafkaConsumer(kafka_topic, bootstrap_servers=[kafka_bootstrap_servers])
+    # for message in consumer:
+    #     print(message.value.decode())
+        
+    # Create a MinIO client
+    minio_client = Minio(
+        'http://minio-service.minio.svc.cluster.local:9000',
+        access_key='efSZjPmIfTtGUH7NKP4H',
+        secret_key='m5CwKsaFxXMxYwZHH3wu6gPlCf0Q7WrNWxkcdA97',
+        secure=False  # Set to True if using HTTPS   
+    )
 
-    consumer = KafkaConsumer(kafka_topic, bootstrap_servers=[kafka_bootstrap_servers])
-    for message in consumer:
-        print(message.value.decode())
+    # Get the object from the bucket
+    response = minio_client.get_object('logs', 'kafka-quickstart-processor/kafka-quickstart-processor-1.txt')
 
+    # Read the file line by line
+    for line in response.stream(decode_content=True).splitlines():
+        print(line.decode('utf-8'))  # Print each line
+
+    response.close()
+    response.release_conn()
 
     # Initialize Kafka client
     # kafka_client = KafkaClient(kafka_bootstrap_servers, kafka_topic)
